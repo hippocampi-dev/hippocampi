129139e87152a51cf8da19f5dd5867fb3c747f3d
WaitHoldMyBeer <kenanblair2@gmail.com>
2025-02-04 21:12:42 -0800

    Updated dashboard page to allow deployment on fork

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,7 @@
 // dif view for user if patient or doctor --> create components for each
 
 // ignore /account and /checkout, both are for payment
+
+export default function Dashboard() {
+  return <main></main>;
+}
