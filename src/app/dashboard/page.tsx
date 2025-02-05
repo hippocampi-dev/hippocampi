db284f98308d1f308afaf19f359703fb47f03b16
Travis Dao <travisdao.t@gmail.com>
2025-02-04 21:50:16 -0800

    temp remove stripe (eslint annoying af)

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,9 @@
 // dif view for user if patient or doctor --> create components for each
 
 // ignore /account and /checkout, both are for payment
+
+export default function Dashboard() {
+  return (
+    <main></main>
+  )
+}
