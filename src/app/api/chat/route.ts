49af75c0300101b3b5e8cd087a0aaeaf0c727cb4
WaitHoldMyBeer <kenanblair2@gmail.com>
2025-02-21 22:07:07 -0800

    chatbot patient side (utils)

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,14 +1,16 @@
 import { streamText } from "ai";
-import { openai } from "@ai-sdk/openai";
+import { openai } from "~/utilities/openai";
+
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
 
 export async function POST(req: Request) {
   const { messages } = await req.json();
-  console.log(messages);
+
   const result = streamText({
     model: openai("gpt-4o"),
-    system: "You are a helpful assistant.",
     messages,
   });
-  console.log(result.toDataStreamResponse());
+
   return result.toDataStreamResponse();
 }
